c64b3e81d8b04cfe0514812083d3b68bda21ce36
Jamie1jia <40625556+Jamie1jia@users.noreply.github.com>
2019-11-14 14:53:28 +0800

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,9 @@
+#第一步：调用pandas包
+import pandas as pd
+import os
+# iris_concat = pd.DataFrame()
+# # 第二步：读取数据并合并
+# for name in os.listdir('./data/'):
+#     iris = pd.read_excel('./data/%s'%(name))
+#     iris_concat = pd.concat([iris_concat,iris1])
+# iris_concat.to_excel('./iris_concat.xlsx')#数据保存路径   
